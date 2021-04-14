2e9bedcdf332b04cd0c145160499767f79251952
zhoukuo123 <1178053078@qq.com>
2021-04-14 18:42:24 +0800

    filter-dev update

--- src/main/java/org/xiyoulinux/freetest/filter/DeviceAdapterFilter.java
+++ src/main/java/org/xiyoulinux/freetest/filter/DeviceAdapterFilter.java
@@ -1,15 +1,35 @@
 package org.xiyoulinux.freetest.filter;
 
 import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
  * @author CoderZk
  */
+@WebFilter(filterName = "DeviceAdapterFilter", urlPatterns = "/index.html")
 public class DeviceAdapterFilter implements Filter {
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void init(FilterConfig filterConfig) throws ServletException {
 
-        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        String userAgent = req.getHeader("user-agent");
+        if (userAgent.contains("iPhone") || userAgent.contains("Android") || userAgent.contains("iPad")) {
+            resp.sendRedirect("mobile_access.html");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
     }
 }
